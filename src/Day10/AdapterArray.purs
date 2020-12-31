830217e569ef90c226de56e6812f68d451094592
Spencer Dent <mr.spencerdent@gmail.com>
2020-12-31 09:08:27 -0500

    rework part 1 to prep for part 2 start work on part 2
    There seems to be a major flaw in my logic.
    It works on the smaller test input,
    but it is nearly an entire order of magnitude off on the second.
    I am projecting that it will be multiple orders of magnitude off on the actual input.
    (possibly 2 or 3)
    
    Looking at the results, I believe that my grouping and counting on those groups
    are the culprits in the miscalculation.
    
    In long chains of 1-gaps, I cut off at each of the 3-gaps.
    There could be many miscounted permutations bridged over those gaps.

--- src/Day10/AdapterArray.purs
+++ src/Day10/AdapterArray.purs
@@ -1,75 +1,82 @@
 module Day10.AdapterArray where
 
 import Prelude
+import Data.Int (toNumber)
+import Data.List (List(..), drop, filter, foldl, fromFoldable, last, length, snoc, sort, span, union, (:))
+import Data.Maybe (fromMaybe)
 import Data.Number (fromString)
-import Data.List (List(..), fromFoldable, sort, (:))
-import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String (Pattern(..), split, trim)
 
 testInput :: String
 testInput = "16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4\n"
 
 testList :: List Number
-testList = fromFoldable $ (fromMaybe 0.0 <<< fromString) <$> (split (Pattern "\n") (trim testInput))
+testList = numberList testInput
 
-type JoltGapTracker
-  = { one :: Number
-    , two :: Number
-    , three :: Number
-    }
+testBag :: List Number
+testBag = adapterBag testList
 
-getOne :: JoltGapTracker -> Number
-getOne { one } = one
+testBag2 :: List Number
+testBag2 = adapterBag $ numberList "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
 
-incOne :: JoltGapTracker -> JoltGapTracker
-incOne jgt = { one: jgt.one + 1.0, two: jgt.two, three: jgt.three }
+numberList :: String -> List Number
+numberList input = fromFoldable $ (fromMaybe 0.0 <<< fromString) <$> (split (Pattern "\n") (trim input))
 
-getTwo :: JoltGapTracker -> Number
-getTwo { two } = two
-
-incTwo :: JoltGapTracker -> JoltGapTracker
-incTwo jgt = { one: jgt.one, two: jgt.two + 1.0, three: jgt.three }
-
-getThree :: JoltGapTracker -> Number
-getThree { three } = three
-
-incThree :: JoltGapTracker -> JoltGapTracker
-incThree jgt = { one: jgt.one, two: jgt.two, three: jgt.three + 1.0 }
-
-countJoltGaps :: List Number -> Maybe JoltGapTracker
-countJoltGaps Nil = Nothing
-
-countJoltGaps numList = countJoltGaps' { one: 0.0, two: 0.0, three: 0.0 } modifiedList
+joltGapList :: forall a. Ring a => List a -> List a
+joltGapList = joltGapList' Nil
   where
-  modifiedList :: List Number
-  modifiedList = 0.0 : (sort numList)
+  joltGapList' :: List a -> List a -> List a
+  joltGapList' acc (first : rest@(second : _)) = joltGapList' (snoc acc (second - first)) rest
 
-  countJoltGaps' :: JoltGapTracker -> List Number -> Maybe JoltGapTracker
-  countJoltGaps' acc Nil = Just acc
+  joltGapList' acc _ = acc
 
-  countJoltGaps' acc (_ : Nil) = Just (incThree acc)
+-- FIXME: I'm assuming that there's an equation I can substitute
+-- FIXME: refactor name
+groupPermutationCount :: forall a. List a -> Number
+groupPermutationCount list = case length list of
+  1 -> 1.0
+  2 -> 1.0
+  3 -> 2.0
+  4 -> 4.0
+  _ -> 0.0
 
-  countJoltGaps' acc (first : restOfInput@(second : _)) = case second - first of
-    1.0 -> countJoltGaps' (incOne acc) restOfInput
-    2.0 -> countJoltGaps' (incTwo acc) restOfInput
-    3.0 -> countJoltGaps' (incThree acc) restOfInput
-    _ -> Nothing
+-- FIXME: I think this might be where the issue is occurring
+reachGroups :: List Number -> List (List Number)
+reachGroups = reachGroups' Nil
+  where
+  reachGroups' :: List (List Number) -> List Number -> List (List Number)
+  reachGroups' acc Nil = acc
+
+  reachGroups' acc (hd : tl) =
+    (\{ init, rest } -> reachGroups' (union acc ((hd : init) : Nil)) (drop (length init - 1) tl))
+      $ span (\num -> num <= (hd + 3.0)) tl
+
+adapterBag :: List Number -> List Number
+adapterBag numList = fromMaybe Nil $ snoc (0.0 : sortedList) <$> (add 3.0 <$> (last sortedList))
+  where
+  sortedList = sort numList
 
 inputPath :: String
 inputPath = "./data/Day10/input.txt"
 
 -- NOTE: answer: 3000
 getSolutionPart1 :: List Number -> Number
-getSolutionPart1 numberList = (getOne joltGaps) * (getThree joltGaps)
+getSolutionPart1 numList = (gapCount 1.0) * (gapCount 3.0)
   where
-  joltGaps :: JoltGapTracker
-  joltGaps = fromMaybe { one: 1.0, two: 0.0, three: -1.0 } $ countJoltGaps numberList
+  joltGaps :: List Number
+  joltGaps = joltGapList $ adapterBag numList
 
-getSolutionPart2 :: List Number -> Number
-getSolutionPart2 numberList = -2.0
+  gapCount :: Number -> Number
+  gapCount = eq >>> flip filter joltGaps >>> length >>> toNumber
+
+-- NOTE: 2199023255552 too low
+-- NOTE: using the other test case, I'm projecting I'm way off
+-- NOTE: 20624432955392 is also too low
+getSolutionPart2 :: List Number -> String
+getSolutionPart2 = adapterBag >>> reachGroups >>> foldl (\product group -> product * (groupPermutationCount group)) 1.0 >>> show
 
 getSolutions :: String -> String
-getSolutions input = "Part 1: " <> show (getSolutionPart1 numberList) <> "\nPart 2: " <> show ""
+getSolutions input = "Part 1: " <> show (getSolutionPart1 numList) <> "\nPart 2: " <> getSolutionPart2 numList
   where
-  numberList :: List Number
-  numberList = fromFoldable $ (fromMaybe 0.0 <<< fromString) <$> (split (Pattern "\n") (trim input))
+  numList :: List Number
+  numList = numberList input
